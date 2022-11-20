4b979d244ad2b74c1774a9ba96210bf36df58ae9
guiisn <guilhermesouza96910524@gmail.com>
2022-11-20 09:11:25 -0300

    chore: add theme in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { MantineProvider } from '@mantine/core';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { allRoutes, RoutesType } from './common/config/routes';
+import { theme } from './common/styles/theme';
 
 const queryClient = new QueryClient();
 
@@ -9,13 +11,19 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      <BrowserRouter>
-        <Routes>
-          {allRoutes.map((route: RoutesType, index: number) => (
-            <Route path={route.path} key={index} element={<route.element />} />
-          ))}
-        </Routes>
-      </BrowserRouter>
+      <MantineProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            {allRoutes.map((route: RoutesType, index: number) => (
+              <Route
+                path={route.path}
+                key={index}
+                element={<route.element />}
+              />
+            ))}
+          </Routes>
+        </BrowserRouter>
+      </MantineProvider>
     </QueryClientProvider>
   );
 }
